226192149ca265447b6cb1d6f4978e8becdc55ba
K.Kshnyakin <kocmoc007@gmail.com>
2020-10-30 21:26:03 +0300

    1. Загрузка данных в вид [#382544]

--- src/main/java/ru/job4j/car_accident/controllers/IndexController.java
+++ src/main/java/ru/job4j/car_accident/controllers/IndexController.java
@@ -4,11 +4,18 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.ArrayList;
+
 @Controller
 public class IndexController {
 
     @GetMapping("/")
     public String index(Model model) {
+        ArrayList<String> users = new ArrayList<>();
+        users.add("KOstya");
+        users.add("Danya");
+        users.add("Vlad");
+        model.addAttribute("users", users);
         return "index";
     }
 }
